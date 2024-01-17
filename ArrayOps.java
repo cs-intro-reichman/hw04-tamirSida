2a014c95ee32b12ec0999d10f6ebe2da9a84a408
Tamir Sida <tamirsida@Tamirs-MacBook-Pro.local>
2024-01-17 21:57:22 +0200

    v3

--- ArrayOps.java
+++ ArrayOps.java
@@ -19,28 +19,53 @@ public class ArrayOps {
         return totalSum;
     }
 
-    public static int maxValue(int [] array) {
+    //create an array to store the number and its index
+    public static int [] maxValueIndex(int [] array) {
         int [] arr = array;
         int n = arr.length;
         int bignum = arr[0];
+        int index=0;
+        int [] bigindex = new int [2];
         for(int i=0; i<n; i++){
             if(arr[i]>bignum){
                 bignum=arr[i];
+                index =i;
             }
         }
-        return bignum;
+        arr[0]=bignum;
+        arr[1]=index;
+        return bigindex;
     }
 
-    public static int secondMaxValue(int [] array) {
-        int [] arr = array;
-        int n = arr.length;
-        int bignum = maxValue(array);
-        int secondNum= 0;
-        for(int i=0; i<n; i++){
-            if(arr[i]>secondNum && arr[i]<=bignum){
-                secondNum=arr[i];
+    public static boolean has2Max (int [] arr){
+        int []bigarr =maxValueIndex(arr);
+        //get the big number from array
+        int bigNum=bigarr[0];
+        //get the index of it from array
+        int indexOfBig= bigarr[1];
+        for(int i=indexOfBig+1;i<arr.length;i++){
+            if(arr[i]==bigNum){
+                return true;
             }
         }
+        return false;
+    }
+
+    public static int secondMaxValue(int [] arr) {
+        int n = arr.length;
+        int [] bigarr= maxValueIndex(arr);
+        int bignum = bigarr[1];
+        int secondNum= 0;
+        if (has2Max(arr)) {
+            secondNum=bignum;
+        }
+        else{
+            for(int i=0; i<n; i++){
+                if(arr[i]>secondNum && arr[i]<bignum){
+                secondNum=arr[i];
+                }
+             }
+            }
         return secondNum;
     }
 
